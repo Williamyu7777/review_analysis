5946125aef679a6d1e770d14820d946a60765706
williamyu7 <yu_ren_hua@hotmail.com>
2023-05-07 13:00:02 +0800

    delete count

--- read.py
+++ read.py
@@ -1,9 +1,7 @@
 data = []
-count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line.strip())
-        count =+ 1
         if len(data) % 1000 == 0:
         	print(len(data))
 print('total:', len(data), 'comments')
